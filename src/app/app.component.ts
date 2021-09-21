d9d08f906a50ec05ad72a6bbfc8c43ccf18cf0fd
Paul'ius <stuksys.paulius@gmail.com>
2021-09-21 18:07:06 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { EmployeeService } from './employee.service';
 export class AppComponent implements OnInit {
   public employees!: Employee[];
   public editEmployee!: Employee;
+  public deleteEmployee!: Employee;
 
   constructor(private employeeService: EmployeeService) { }
 
@@ -47,6 +48,7 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#updateEmployeeModal');
     }
     if (mode === 'delete') {
+      this.deleteEmployee = employee;
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
     container?.appendChild(button);
@@ -79,6 +81,34 @@ export class AppComponent implements OnInit {
         alert(error.message)
       },
     );
+  }
+
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      },
+    );
+  }
+
+  public searchEmployees(key: string): void{
+    const results: Employee[] = [];
+    for(const employee of this.employees){
+      if(employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phoneNumber.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if(results.length === 0 || !key) {
+      this.getEmployees();
+    }
   }
 
 }
